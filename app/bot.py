cb8d78ccf108ce98a837d9a0422dbbd62eb36180
dmitry_h <113709033+dmitryh2004@users.noreply.github.com>
2024-02-18 20:49:13 +0300

    0.8

--- app/bot.py
+++ app/bot.py
@@ -12,6 +12,7 @@ from app.dialogs import msg
 from database import databaseEntity as db
 from database import usernameDatabaseEntity as udb
 from database import groupsDatabaseEntity as gdb
+from database import promoDatabaseEntity as pdb
 
 import random
 
@@ -42,19 +43,21 @@ def change_dimension(group, user, item):
     min_offset = int(ITEMS_MIN_OFFSET[item])
     max_offset = int(ITEMS_MAX_OFFSET[item])
     offset = random.randint(min_offset, max_offset)
+    bonus = db.getBonus(group, user, item)
+    db.useBonus(group, user, item)
     if offset >= 0:
-        if (dimension + offset) > ITEMS_MAX_VALUE[item]:
+        if (dimension + offset + bonus) > ITEMS_MAX_VALUE[item]:
             db.setDimension(group, user, item, ITEMS_MAX_VALUE[item])
-            offset = ITEMS_MAX_VALUE[item] - dimension
+            offset = ITEMS_MAX_VALUE[item] - dimension - bonus
         else:
-            db.setDimension(group, user, item, dimension + offset)
+            db.setDimension(group, user, item, dimension + offset + bonus)
     else:
-        if (dimension + offset) < ITEMS_MIN_VALUE[item]:
+        if (dimension + offset + bonus) < ITEMS_MIN_VALUE[item]:
             db.setDimension(group, user, item, ITEMS_MIN_VALUE[item])
-            offset = dimension - ITEMS_MIN_VALUE[item]
+            offset = (dimension + bonus) - ITEMS_MIN_VALUE[item]
         else:
-            db.setDimension(group, user, item, dimension + offset)
-    return (dimension + offset), offset
+            db.setDimension(group, user, item, dimension + offset + bonus)
+    return (dimension + offset + bonus), offset
 
 
 @dp.message(aiogram.F.text)
@@ -76,7 +79,7 @@ async def handle_message(message: types.message.Message):
     else:
         # добавить имя юзера в базу данных юзернеймов
         gdb.append(str(group), str(group_name))
-        udb.append(str(user_id), str(user_name))
+        udb.append(str(user_id), str(user_name), udb.getUserRole(user_id))
         if text == "/make_turn":
             dim = db.getCurrent(str(group), str(user_id))
             current_time = datetime.datetime.now()
@@ -126,6 +129,143 @@ async def handle_message(message: types.message.Message):
             else:
                 img_path = aiogram.types.FSInputFile(db.form_piechart_top_10(str(group), dim))
                 await bot.send_photo(group, img_path, thread)
+        # здесь начинается 0.8
+        elif text == "/my_role":
+            if group == user_id:
+                role_id = udb.getUserRole(user_id)
+                role = config.ROLES[role_id][0]
+                desc = config.ROLES[role_id][1]
+                await bot.send_message(group, msg.my_role.format(name=user_name, role=role, desc=desc), thread)
+            else:
+                await bot.send_message(group, msg.my_role_not_in_groups.format(name=user_name), thread)
+        elif text == "/my_id":
+            if group == user_id:
+                await bot.send_message(group, msg.my_id.format(name=user_name, user_id=user_id), thread)
+            else:
+                await bot.send_message(group, msg.my_id_not_in_groups.format(name=user_name), thread)
+        elif text == "/group_id":
+            if group == user_id:
+                await bot.send_message(group, msg.group_id_only_in_groups.format(name=user_name), thread)
+            else:
+                await bot.send_message(group, msg.group_id.format(name=user_name, group_id=group), thread)
+        elif text == "/help_promo":
+            if group == user_id:
+                await bot.send_message(group, msg.help_promo, thread)
+            else:
+                await bot.send_message(group, msg.help_promo_not_in_groups.format(name=user_name), thread)
+        elif text.split(" ")[0] == "/promo":
+            if group == user_id:
+                try:
+                    promo_id = text.split(" ")[1]
+                    target_group_id = text.split(" ")[2]
+                    if not (promo_id in pdb.database):
+                        await bot.send_message(group, msg.promo_error.format(name=user_name, promo_id=promo_id, desc="Такого промокода не существует."), thread)
+                    else:
+                        if not (target_group_id in db.database):
+                            await bot.send_message(group, msg.promo_error.format(name=user_name, promo_id=promo_id,
+                                                                                 desc=f"Группы с ID {target_group_id} нет в базе данных бота."),
+                                                   thread)
+                        else:
+                            success, promo_message = db.usePromo(str(target_group_id), str(user_id), str(promo_id))
+                            if success:
+                                await bot.send_message(group, msg.promo_success.format(name=user_name, desc=promo_message), thread)
+                            else:
+                                await bot.send_message(group, msg.promo_error.format(name=user_name, promo_id=promo_id,
+                                                                                     desc=promo_message), thread)
+                except IndexError as e:
+                    await bot.send_message(group, msg.invalid_syntax.format(desc=""), thread)
+
+            else:
+                await bot.send_message(group, msg.promo_not_in_groups.format(name=user_name), thread)
+        # 0.8 - команды админа
+        elif text == "/admin_features":
+            if group == user_id:
+                role = udb.getUserRole(user_id)
+                if role != 2:
+                    await bot.send_message(group, msg.unknown, thread)
+                else:
+                    await bot.send_message(group, msg.admin_help, thread)
+            else:
+                await bot.send_message(group, msg.unknown, thread)
+        elif text == "/developer_features":
+            if group == user_id:
+                role = udb.getUserRole(user_id)
+                if role != 3:
+                    await bot.send_message(group, msg.unknown, thread)
+                else:
+                    await bot.send_message(group, msg.dev_help, thread)
+            else:
+                await bot.send_message(group, msg.unknown, thread)
+        elif text.split()[0] == "/add_promo":
+            if group == user_id and udb.getUserRole(user_id) >= 2:
+                try:
+                    promo, bonus, promo_uses, min_role = text.split()[1], text.split()[2], text.split()[3], text.split()[4]
+                    if not (promo in pdb.database):
+                        if not (int(min_role) in range(0, 4)):
+                            await bot.send_message(group, msg.invalid_syntax.format(desc="Указана неверная минимальная роль (допустимые значения: 0,1,2,3)"), thread)
+                        if not (int(promo_uses) > 0):
+                            await bot.send_message(group, msg.invalid_syntax.format(
+                                desc="Число использований должно быть целым положительным числом"), thread)
+                        pdb.setPromo(promo, int(bonus), int(promo_uses), int(min_role))
+                        await bot.send_message(group, msg.add_promo_success.format(name=user_name, promo_id=promo, promo_uses=promo_uses, min_role=config.ROLES[int(min_role)][0]))
+                    else:
+                        await bot.send_message(group, msg.add_promo_already_exists.format(name=user_name, promo_id=promo), thread)
+                except IndexError as e:
+                    await bot.send_message(group, msg.invalid_syntax.format(desc=""), thread)
+            else:
+                await bot.send_message(group, msg.unknown, thread)
+        elif text.split()[0] == "/remove_promo":
+            if group == user_id and udb.getUserRole(user_id) >= 2:
+                try:
+                    promo = text.split()[1]
+                    if not (promo in pdb.database):
+                        await bot.send_message(group,
+                                               msg.remove_promo_dont_exist.format(name=user_name, promo_id=promo),
+                                               thread)
+                    else:
+                        pdb.database.pop(promo)
+                        await bot.send_message(group,
+                                               msg.remove_promo_success.format(name=user_name, promo_id=promo),
+                                               thread)
+                except IndexError as e:
+                    await bot.send_message(group, msg.invalid_syntax.format(desc=""), thread)
+            else:
+                await bot.send_message(group, msg.unknown, thread)
+        elif text.split()[0] == "/set_role":
+            if group == user_id and udb.getUserRole(user_id) >= 2:
+                max_role_to_set = 1 if udb.getUserRole(user_id) == 2 else 2
+                try:
+                    player, role = text.split()[1], int(text.split()[2])
+                    if not (role in range(0, 4)):
+                        await bot.send_message(group, msg.invalid_syntax.format(
+                            desc="Указана неверная роль (допустимые значения: 0,1,2,3)"), thread)
+                    else:
+                        if role > max_role_to_set:
+                            await bot.send_message(group, msg.invalid_syntax.format(
+                                desc=f"Указанная роль выше максимальной роли, которую вы можете назначить ({max_role_to_set})"), thread)
+                        else:
+                            if not (player in udb.database):
+                                await bot.send_message(group, msg.set_role_fail.format(
+                                        name=user_name, desc=f"Игрок с ID {player} ни разу не пользовался ботом."),
+                                                       thread)
+                            else:
+                                if player == str(user_id):
+                                    await bot.send_message(group, msg.set_role_fail.format(
+                                        name=user_name, desc=f"Вы не можете изменить свою роль."),
+                                                           thread)
+                                else:
+                                    if udb.getUserRole(player) > max_role_to_set:
+                                        await bot.send_message(group, msg.set_role_fail.format(
+                                            name=user_name, desc=f"Роль игрока с ID {player} выше или равна вашей."),
+                                                               thread)
+                                    else:
+                                        udb.database[player]["role"] = role
+                                        await bot.send_message(group, msg.set_role_success.format(name=user_name, target=player, target_name=udb.getUsername(player),
+                                                                                              role=config.ROLES[role][0], role_desc=config.ROLES[role][1]), thread)
+                except IndexError as e:
+                    await bot.send_message(group, msg.invalid_syntax.format(desc=""), thread)
+            else:
+                await bot.send_message(group, msg.unknown, thread)
         else:
             await bot.send_message(group, msg.unknown, thread)
 
